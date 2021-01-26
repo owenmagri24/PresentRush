e5ef815f0ba2c85a650b31d27f94b399250884c3
owenmagri24 <74650731+owenmagri24@users.noreply.github.com>
2021-01-26 11:52:33 +0100

    Player sprite animation and direction
    -Added player sprites and animations
    -Plays idle animation when not moving
    -Plays walk animation when moving
    -Player sprite flips direction depending on where player is moving

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,14 @@ public class Player : MonoBehaviour
 {
     //protecting value from changes done by players
     //only designers will be able to change the value
+    public Animator animator; //variable used to call animator
+
     [SerializeField]
     //setting player movement speed
-
     public float speed = 6.5f;
     private GameManager _gameManager;
+
+    private bool facingRight = true; //variable used for direction facing
 
     // Start is called before the first frame update
     void Start()
@@ -37,12 +40,28 @@ public class Player : MonoBehaviour
     void playerMovement()
     {
         //moving character with WASD / Arrow keys horizontally and vertically
-        //will try and make it 4 directional movement only if there is enough time
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        //if both horizontal and vertical values are 0, plays the idle animation, else play walking animation
+        if(horizontal == 0 && vertical == 0){
+            animator.SetBool("isRunning", false);
+        }
+        else {
+            animator.SetBool("isRunning", true);
+        }
+
         Vector3 direction = new Vector3(horizontal,vertical,0);
         transform.Translate(direction * speed * Time.deltaTime);
+
+
+        //loop to flip depending on horizontal value and direction facing
+        if(horizontal > 0 &&!facingRight){
+            flip();
+        }
+        else if(horizontal < 0 && facingRight){
+            flip();
+        }
 
         //player boundaries / walls
 
@@ -70,6 +89,14 @@ public class Player : MonoBehaviour
             transform.position = new Vector3 (transform.position.x,-4.4f,0);
         }
 
-        /*player movement deduction*/
+    }
+
+    //Function to flip scale
+    void flip(){
+        //changes facingRight to false, gets localscale and multipies x value by -1 and applies it to localScale again.
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
